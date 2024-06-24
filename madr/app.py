70ac0f4550e255f66145b9a9313d3df905c4a835
2005869 <2005869@aluno.univesp.br>
2024-06-24 12:15:58 -0300

    register user without database

--- madr/app.py
+++ madr/app.py
@@ -1,6 +1,8 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-from madr.schemas import Message
+from madr.schemas import Message, User, UserPublic
 
 app = FastAPI()
 
@@ -8,3 +10,8 @@ app = FastAPI()
 @app.get('/', response_model=Message)
 def index_response():
     return {'message': 'home do projeto'}
+
+
+@app.post('/contas', response_model=UserPublic, status_code=HTTPStatus.CREATED)
+def register_user(user: User):
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,25 @@
 from http import HTTPStatus
 
+usertest = {
+    'username': 'bobmarley',
+    'email': 'bob@marley.com',
+    'senha': 'secret',
+}
+
+userresponse = {
+    'username': 'bobmarley',
+    'email': 'bob@marley.com',
+}
+
 
 def test_index_recebe_mensagem(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'home do projeto'}
+
+
+def test_register_user(client):
+    response = client.post('/contas', json=usertest)
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == userresponse
